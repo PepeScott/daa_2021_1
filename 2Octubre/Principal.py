60561881a495c3029bbc2d6e643efb1eab1ff995
PepeScott <71674466+PepeScott@users.noreply.github.com>
2020-10-02 11:09:21 -0500

    Update Principal.py

--- 2Octubre/Principal.py
+++ 2Octubre/Principal.py
@@ -18,24 +18,3 @@ def main():
         suma+=a3.get_item(i-2017,e,m)
     print("El promedio anual es de: ",suma/34)
     print("La suma es de: ", suma)
-    #tarea3 estado
-    suma=0.0
-    for j in range(1985,2019,1):
-        for i in range(0,13,1):
-            #print("anio",j,"mes",i,a3.get_item(j-1985,e,i))
-            if i!=0:
-                suma+=a3.get_item(j-1985,e,i)
-        #print()
-    print("promedio",suma/408)
-    print("La suma es de:")
-    #tarea4 todo
-    suma=0.0
-    for k in range(2017,2019,1):
-        for i in range(1,33,1):
-            for j in range(0,13,1):
-                if j!=0:
-                    #print("en el año",k,"en el mes",a3.get_item(k-1985,0,j),"en el estado",a3.get_item(a-1985,i,0),"precipitacion",a3.get_item(k-1985,i,j))
-                    suma+=a3.get_item(k-1985,i,j)
-    print(suma/13056)
-                    
-main()
