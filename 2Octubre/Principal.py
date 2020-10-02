34c96c880bb1ee4313659e405cbcbc29d9e93a7a
PepeScott <71674466+PepeScott@users.noreply.github.com>
2020-10-02 12:31:29 -0500

    Update Principal.py

--- 2Octubre/Principal.py
+++ 2Octubre/Principal.py
@@ -18,3 +18,42 @@ def main():
         suma+=a3.get_item(a-2017,e,c)
     print("El promedio anual es de: ",suma/34)
     print("La suma es de: ", suma)
+    datoMayor17=0
+    estadoM17=0
+    mesM17=0
+    datoMenor17=0
+    estadoMe17=0
+    mesMe17=0
+    datoMayor18=0
+    estadoM18=0
+    mesM18=0
+    datoMenor18=0
+    estadoMe18=0
+    mesMe18=0
+    for anio in range(2017,2019,1):
+        for r in range(2,33,1):
+            for c in range(1,13,1):
+                dato=int(a3.get_item(anio-2017,r,c))
+                if anio==2017:
+                    if dato > datoMayor17:
+                        datoMayor17=dato
+                        estadoM17=r
+                        mesM17=c
+                    if dato < datoMayor17:
+                        datoMenor17=dato
+                        estadoMe17=r
+                        mesMe17=c
+                else:
+                    if dato > datoMayor18:
+                        datoMayor18=dato
+                        estadoM18=r
+                        mesM18=c
+                    if dato < datoMayor18:
+                        datoMenor18=dato
+                        estadoMe18=r
+                        mesMe18=c
+    print("El mes que mas llovio fue",a3.get_item(0,0,mesM17),"en el estado numero",a3.get_item(0,estadoM17,0),"con un total de ", datoMayor17,"en el anio 2017")
+    print("El mes que mas llovio fue",a3.get_item(1,0,mesM18),"en el estado numero",a3.get_item(1,estadoM18,0),"con un total de ", datoMayor18,"en el anio 2018")
+
+    print("El mes que meno llovio fue",a3.get_item(0,0,mesMe17),"en el estado numero",a3.get_item(0,estadoMe17,0),"con un total de ", datoMenor17,"en el anio 2017")
+    print("El mes que meno llovio fue",a3.get_item(1,0,mesMe18),"en el estado numero",a3.get_item(1,estadoMe18,0),"con un total de ", datoMenor18,"en el anio 2018")
